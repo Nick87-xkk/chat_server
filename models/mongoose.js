157f9741e953fb42b2affa5d752658895dc980ff
weilong <weilong1159@outlook.com>
2022-04-06 10:59:27 +0800

    delete: 删除mongodb

--- models/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-const MONGO_URL = "mongodb://nick87:12138@49.232.185.124:27017/?authSource=admin"
-
-mongoose.connect(MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true },(err,data)=>{
-    if(err) console.log(err);
-    console.log(data)
-})
