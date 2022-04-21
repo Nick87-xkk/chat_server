cec20f6f702d0cfd2627548ffdfc0df3958a394d
Nick87-xkk <59241530+Nick87-xkk@users.noreply.github.com>
2022-04-21 10:46:05 +0800

    feat: 数据库逻辑图

--- socketio.js
+++ socketio.js
@@ -1,43 +1,48 @@
-var socketio = {};
-var socket_io = require('socket.io');
+const socketio = {};
+const socket_io = require('socket.io');
 
 // 存储account对应socketid
 const chatMap = new Map();
 
 //获取io
 socketio.getSocketio = function (server) {
-  var io = socket_io(server, {
-    cors: true
-  });
-
-  io.on('connection', (socket) => {
-    /* console.log('a user connected');
-    console.log(socket.handshake.query);
-    console.log(socket.id); */
-    // console.log(io.sockets.adapter.rooms);
-    // 向除了自己以外的客户端推送消息
-    chatMap.set(socket.handshake.query.account,socket.id);
-    console.log(chatMap);
-    // console.log(socket.id,socket.handshake.query.account);
-    socket.on('chat message',async (msg) => {
-      // console.log(msg.receiveAccount);
-
-      // console.log(chatMap.get(msg.receiveAccount.toString())); 
-
-    //  console.log(await studentModel.selectById(1001)); 
-      // 发送给对应用户
-      // socket.to(chatMap.get(msg.receiveAccount.toString().toString())).emit('chat message', `${socket.handshake.query.account}: ${msg.data}`);
-
-      socket.broadcast.emit('chat message', `${socket.handshake.query.account}: ${msg.data}`);
-   
-    
+    const io = socket_io(server, {
+        cors: true
     });
-    socket.on('video message',(msg)=>{
-      console.log(msg);
-      socket.emit('video message',msg.data)
-    })
-  });
- 
+
+    io.on('connection', (socket) => {
+        /* console.log('a user connected');
+        console.log(socket.handshake.query);
+        console.log(socket.id); */
+        // console.log(io.sockets.adapter.rooms);
+        // 向除了自己以外的客户端推送消息
+        chatMap.set(socket.handshake.query.account, socket.id);
+        // console.log(chatMap);
+        // console.log(socket.id,socket.handshake.query.account);
+        socket.on('chat message', async (msg) => {
+            // console.log(msg.receiveAccount);
+
+            // console.log(chatMap.get(msg.receiveAccount.toString()));
+
+            //  console.log(await studentModel.selectById(1001));
+            // 发送给对应用户
+            // socket.to(chatMap.get(msg.receiveAccount.toString().toString())).emit('chat message', `${socket.handshake.query.account}: ${msg.data}`);
+
+            socket.broadcast.emit('chat message', `${socket.handshake.query.account}: ${msg.data}`);
+
+
+        });
+        socket.on('visitor', (msg) => {
+            // console.log(msg)
+            socket.broadcast.emit('visitor', msg);
+        })
+
+        socket.on('video message', (msg) => {
+            console.log(msg);
+            socket.emit('video message', msg.data)
+        })
+    });
+
 };
 
 /* 
