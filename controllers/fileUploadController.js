6b00b7aebc71b98d0b51c154814a98676cc20ccf
weilong <59241530+Nick87-xkk@users.noreply.github.com>
2022-05-20 03:10:12 +0800

    feat: 完成文件上传下载

--- controllers/fileUploadController.js
+++ controllers/fileUploadController.js
@@ -2,47 +2,60 @@ const formidable = require("formidable")
 const {log} = require("debug");
 const path = require("path");
 const fs = require('fs')
+const md5 =require('js-md5')
 const fileController = {
     upLoad: async (req,res)=>{
+        //文件夹下已有文件的名称
+        let dirFilesName = fs.readdirSync('./public/upfiles/');
+
         let form = new formidable.IncomingForm();
         form.encoding = 'utf-8' // 编码
         form.keepExtensions = true // 保留扩展名
         form.uploadDir = path.join(__dirname,'../public/upfiles/') // 文件存储路径
-        form.parse(req,(err,fileds,files)=>{
+        form.parse(req,(err,data,files)=>{
             if (err) {
                 return res.json({
                     code:0,
                     message:err
                 })
             }
-
             let oldPath = files.file.filepath;
             let fileName = files.file.originalFilename;
 
-            let newPath = path.join(path.dirname(oldPath),fileName)
-
-            fs.rename(oldPath,newPath,(err)=>{
-                if (err){
-                    return res.json({
-                        code:0,
-                        message:err
+            let fileMd5 = md5(fs.readFileSync(oldPath));
+            let newName = `${fileMd5}.${fileName.split('.')[1]}`;
+            // 查询文件是否存在
+            if (!dirFilesName.includes(newName.toString())){
+                let newPath = path.join(path.dirname(oldPath),newName)
+                fs.rename(oldPath,newPath,(err)=>{
+                    if (err){
+                        return res.json({
+                            code:0,
+                            message:err
+                        })
+                    }
+                    res.json({
+                        code:200,
+                        message:newName
                     })
-                }
-                res.json({
-                    code:200,
-                    message:"up success"
                 })
-            })
-            /*console.log(fileds);
-            console.log(files);*/
-
+            }else{
+                fs.unlink(oldPath,(err)=>{
+                   if(err){
+                       throw err;
+                   }
+                });
+                res.json({
+                    code:201,
+                    message:newName
+                })
+            }
         })
     },
 
     download: async (req,res)=>{
-        res.download('./public/upfiles/test.txt','testfile',(err)=>{
-            console.log(err)
-        })
+        console.log(req.query.name)
+        res.download(`./public/upfiles/${req.query.name}`,req.query.name.toString())
     }
 }
 
