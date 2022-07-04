239897d88ab8c0e7e301e229b916b2232e97a6e2
weilong <59241530+Nick87-xkk@users.noreply.github.com>
2022-07-04 11:02:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,10 @@
 const configs = {
-    /* mysql: {
-      host: '49.232.185.124',
-      port: '3306',
-      user: 'javaee',
-      password: '12138',  // 自己设置的密码
-      database: 'javaee' // 数据库的名字
-    }, */
     mysql: {
-      host: '49.232.185.124',
-      port: '3306',
-      user: 'nick87',
-      password: '666666',  // 自己设置的密码
-      database: 'chattest' // 数据库的名字
+      host: '',
+      port: '',
+      user: '',
+      password: '',  // 自己设置的密码
+      database: '' // 数据库的名字
     },
     // 打印错误
     log: {
